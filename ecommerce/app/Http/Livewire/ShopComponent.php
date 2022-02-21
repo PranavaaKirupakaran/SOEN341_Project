dc44bd9952a02cab6fe3070b55e57df4b0aed09f
G.O'Brien <glanob38@gmail.com>
2022-02-21 14:43:16 -0500

    Worked on issues #37 #38 #39

--- ecommerce/app/Http/Livewire/ShopComponent.php
+++ ecommerce/app/Http/Livewire/ShopComponent.php
@@ -8,6 +8,15 @@ use Livewire\Component;
 
 class ShopComponent extends Component
 {
+    public $sorting;
+
+    public $pagesize;
+
+    public function mount(){
+        $this->sorting = "default";
+        $this->pagesize = 12;
+    }
+
     // use WithPagination;
     public function store($product_id, $product_name, $product_price)
     {
@@ -18,7 +27,23 @@ class ShopComponent extends Component
 
     public function render()
     {
-        $products = Product::paginate(12);
+        if($this->sorting=='date')
+        {
+            $products = Product::orderBy('created_at','DESC')->paginate($this->pagesize);
+        }
+        else if($this->sorting=='price')
+        {
+            $products = Product::orderBy('regular_price','ASC')->paginate($this->pagesize);
+        }
+        else if($this->sorting=='price-desc')
+        {
+            $products = Product::orderBy('regular_price','DESC')->paginate($this->pagesize);
+        }
+        else
+        {
+            $products = Product::paginate($this->pagesize);
+        }
+        //$products = Product::paginate(12);
         return view('livewire.shop-component',['products'=>$products])->layout('layouts.base');
     }
 }
