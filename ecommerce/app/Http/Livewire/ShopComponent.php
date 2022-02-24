9b330a0a87a09f8615866ce7ad55fca7ace0a8ce
G.O'Brien <glanob38@gmail.com>
2022-02-24 08:40:23 -0500

    Worked on fixing bugs on Issue #44

--- ecommerce/app/Http/Livewire/ShopComponent.php
+++ ecommerce/app/Http/Livewire/ShopComponent.php
@@ -2,28 +2,52 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use App\Models\Product;
 use Cart;
 use Livewire\Component;
 
+
 class ShopComponent extends Component
 {
-    public $perPage;
-    public $orderBy = 'price';
-    public $sortBy = 'asc';
+    public $sorting;
+    public $page;
 
+    /*
     public function store($product_id, $product_name, $product_price)
     {
         Cart::add($product_id, $product_name, $product_price)->associate('Apps\Models\Product');
         session()->flash('success_message', 'Item added in Cart');
         return redirect()->route('product.cart');
     }
+*/
+    public function mount()
+    {
+        $this->sorting = "default";
+        $this->page = 12;
+    }
 
     public function render()
     {
-        $products = Product::paginate(12);
-        return view('livewire.shop-component',['products'=>$products])->layout('layouts.base');
-       //     ->orderBy($this->orderBy, $this->sortBy)
-       //     ->paginate($this->perPage)
+        if($this->sorting=="date")
+        {
+            $products = Product::orderBy('created_at', 'ASC')->paginate($this->page);
+        }
+        else if($this->sorting=="price")
+        {
+            $products = Product::orderBy('regular_price', 'ASC')->paginate($this->page);
+        }
+        else if($this->sorting=="price-desc")
+        {
+            $products = Product::orderBy('regular_price', 'DESC')->paginate($this->page);
+        }
+        else
+        {
+            $products = Product::paginate($this->page);
+        }
+
+        $categories = Category::all();
+
+        return view('livewire.shop-component',['products'=>$products, 'categories'=>$categories])->layout('layouts.base');
     }
 }
